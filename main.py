3f64375e9c37acc6a2657731ca070b1f00dd27cc
Pokalnetov Pavel <pokalnetovps@gmail.com>
2023-06-30 07:50:44 +0500

    #2

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ def simpleDiv(num):
     while True:
         n += 1
         if num % div == 0:
-            # divisors.append(div)
-            num = num / div
-            divisors.append(num)
+            divisors.append(div)
+            num = num // div
+            if num!=1: divisors.append(num)
             div = 2
         else:
             div += 1
             if div > num:
-                return divisors, n
+                return sorted(set(divisors)), n
 
 
 def od(num1, num2):
     divisors1 = simpleDiv(num1)[0]
     divisors2 = simpleDiv(num2)[0]
-    print(divisors1,divisors2,sep='\n')
+    print(divisors1, divisors2, sep='\n')
 
     if len(divisors1) == 1 and len(divisors2) == 1:
         if num1 != num2:
@@ -26,13 +26,19 @@ def od(num1, num2):
         else:
             return num1
     else:
-        divisors = set(divisors1) & set(divisors2)
+        divisors = list(set(divisors1) & set(divisors2))
 
     if len(divisors) == 0: return None
 
-    return divisors
+    min = divisors[0]
+    max = min
+    for item in divisors:
+        if item > max: max = item
+        if item < min: min = item
+
+
+    return min, max
 
 
 if __name__ == '__main__':
-
-    print(od(16,32))
+    print(od(512, 1024))
